4ad2865700592850cee93aff35fe5137d5095590
Jose A. Mendez Santiago <jamsmendez02@gmail.com>
2024-08-30 11:46:22 -0600

    settings locals

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -12,8 +12,8 @@ return {
   opts = {
     -- change colorscheme
     -- colorscheme = "astrodark",
-    colorscheme = "darcula-dark",
-    -- colorscheme = "tokyonight-storm",
+    -- colorscheme = "darcula-dark",
+    colorscheme = "tokyonight-storm",
 
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
